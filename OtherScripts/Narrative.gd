e93fe86f14b9381c3e4961bc44461dffa6d9a6b2
RadekGrzankowski <radek.grzankowski@gmail.com>
2024-03-04 02:39:44 +0100

    bugfixes

--- OtherScripts/Narrative.gd
+++ OtherScripts/Narrative.gd
@@ -13,18 +13,18 @@ func _process(delta):
 
 func _on_animation_player_animation_changed(old_name, new_name):
 	if narrative == 5:
-		$".".text = "That is why we want to pass some of the positivity to anyone who would ever play this thing. We've grown. Go make Games! Or go make anything that makes you grow! Grow!"
+		$".".text = "That is why we want to pass some of the positivity to anyone who would ever play this thing. We've grown. So can you! Go make Games! Or go make anything that makes you grow! Grow!"
 	if narrative == 4:
-		$".".text = "Sometimes it is hard, the life only gives us a fixed amount of time to work on ourselves. It can be really exhausting. That is okay, not everything can be achieved, some things really are impossible when you're in a tough situation."
+		$".".text = "Sometimes it is hard, the life only gives us a fixed amount of time to work on ourselves. It can be really exhausting. That is okay, You still have room for something. A place where something beautiful can grow."
 		narrative +=1;
 	if narrative == 3:
 		$".".text = "The beautiful artwork, all the logic, design, and everything else. I am proud of all our work and every contributor of this project. Our skills are growing."
 		narrative +=1;
 	if narrative == 2:
-		$".".text = "Not everyone participated in this specific project, but at this point, we've set up a Discord of aspiring gamedevs and participate in any Gamejams we feel like. Our numbers are growing."
+		$".".text = "Not everyone participated in this specific project, but at this point, we've set up a Discord of aspiring gamedevs and we learn and participate in any Gamejams we feel like. Our numbers are growing."
 		narrative +=1;
 	if narrative == 1:
-		$".".text = "PirateSoftware told us to try and make games, and so we do. I've tried to influence all my colleagues with the love for gamedev and at this point we have 7 people learning how to make games on our discord."
+		$".".text = "PirateSoftware told us to try and make games, and so we do. I've tried to influence all my colleagues with the love for gamedev and at this point 7 new people started making stuff connected to gamedev."
 		narrative +=1;
 	if narrative == 0:
 		$".".text = "We are still on the beginning on our gamedev journey. We're starting really small. We hope that those few minutes you're going to spend here are not going to be wasted. We hope that you'll grow. We hope that we'll grow."
